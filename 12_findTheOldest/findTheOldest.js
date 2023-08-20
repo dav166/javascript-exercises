ff5c3bb49c85b7bab2014684744d7eb7698b5fdd
David <5992264+dav166@users.noreply.github.com>
2023-08-20 05:32:40 -0500

    Initial commit: complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+      const currentYear = new Date().getFullYear();
+      const deathYear = person.yearOfDeath || currentYear;
+      const age = deathYear - person.yearOfBirth;
+  
+      const oldestDeathYear = oldest.yearOfDeath || currentYear;
+      const oldestAge = oldestDeathYear - oldest.yearOfBirth;
+  
+      return age > oldestAge ? person : oldest;
+    });
+  };
+  
+  // Example usage
+  const people = [
+    {
+      name: 'John',
+      birth: 1900,
+      death: 1980
+    },
+    {
+      name: 'Jane',
+      birth: 1910,
+      death: 1990
+    },
+    {
+      name: 'Jim',
+      birth: 1920
+    }
+  ];
+  
+  console.log(findTheOldest(people)); // Object for 'Jane'
+  
 
 // Do not edit below this line
 module.exports = findTheOldest;
