f267c4443d656408022350a7e736e53705cf8202
David <5992264+dav166@users.noreply.github.com>
2023-08-20 05:09:38 -0500

    Initial commit: complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+	// Remove non-alphanumeric characters and convert to lowercase
+	const cleanedString = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+	// Compare the cleaned string with its reverse
+	return cleanedString === cleanedString.split('').reverse().join('');
 };
+
+// Test cases
+console.log(palindromes('racecar')); // true
+console.log(palindromes('tacos')); // false
+console.log(palindromes('A car, a man, a maraca.')); // true
+console.log(palindromes('Rats live on no evil star.')); // true
 
 // Do not edit below this line
 module.exports = palindromes;
