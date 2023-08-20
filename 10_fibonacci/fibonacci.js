97307cf47cbcbd9f8ac61bc10cdb2bac1050f693
David <5992264+dav166@users.noreply.github.com>
2023-08-20 05:17:14 -0500

    Initial commit: finish exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n <= 0) {
+      return "OOPS";
+    }
+    if (n === 1 || n === 2) {
+      return 1;
+    }
+    let a = 1, b = 1, c;
+    for (let i = 3; i <= n; i++) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    return c;
+  };
+  
+  // Test cases
+  console.log(fibonacci(4)); // 3
+  console.log(fibonacci(6)); // 8
 
 // Do not edit below this line
 module.exports = fibonacci;
